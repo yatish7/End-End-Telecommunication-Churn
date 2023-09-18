b1915161dce697992a6c65c1d14ba97f15cb9453
yatish7 <yatish.manne09@gmail.com>
2023-09-18 11:35:42 +0530

    Updated all the files

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-# coding: utf-8
+import sys
+sys.path.append('/Users/yatish/miniconda3/envs/myenv/lib/python3.10/site-packages')
+
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -7,15 +9,16 @@ from sklearn import metrics
 from flask import Flask, request, render_template
 import pickle
 
+
 app = Flask("__name__")
 
-df_1 = pd.read_csv("tel_churn.csv")
+df_1=pd.read_csv("first_telc.csv")
 
 q = ""
 
 @app.route("/")
 def loadPage():
-    return render_template('home.html', query="")
+	return render_template('home.html', query="")
 
 
 @app.route("/", methods=['POST'])
@@ -33,12 +36,17 @@ def predict():
     InternetService
     OnlineSecurity
     OnlineBackup
+    DeviceProtection
     TechSupport
+    StreamingTV
+    StreamingMovies
     Contract
     PaperlessBilling
     PaymentMethod
     tenure
     '''
+    
+
     
     inputQuery1 = request.form['query1']
     inputQuery2 = request.form['query2']
@@ -51,7 +59,10 @@ def predict():
     inputQuery9 = request.form['query9']
     inputQuery10 = request.form['query10']
     inputQuery11 = request.form['query11']
+    inputQuery12 = request.form['query12']
     inputQuery13 = request.form['query13']
+    inputQuery14 = request.form['query14']
+    inputQuery15 = request.form['query15']
     inputQuery16 = request.form['query16']
     inputQuery17 = request.form['query17']
     inputQuery18 = request.form['query18']
@@ -60,12 +71,13 @@ def predict():
     model = pickle.load(open("model.sav", "rb"))
     
     data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5, inputQuery6, inputQuery7, 
-             inputQuery8, inputQuery9, inputQuery10, inputQuery11, inputQuery13, inputQuery16, inputQuery17, inputQuery18, inputQuery19]]
+             inputQuery8, inputQuery9, inputQuery10, inputQuery11, inputQuery12, inputQuery13, inputQuery14,
+             inputQuery15, inputQuery16, inputQuery17, inputQuery18, inputQuery19]]
     
     new_df = pd.DataFrame(data, columns = ['SeniorCitizen', 'MonthlyCharges', 'TotalCharges', 'gender', 
                                            'Partner', 'Dependents', 'PhoneService', 'MultipleLines', 'InternetService',
-                                           'OnlineSecurity', 'OnlineBackup', 'TechSupport',
-                                            'Contract', 'PaperlessBilling',
+                                           'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport',
+                                           'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling',
                                            'PaymentMethod', 'tenure'])
     
     df_2 = pd.concat([df_1, new_df], ignore_index = True) 
@@ -81,7 +93,7 @@ def predict():
     
     new_df__dummies = pd.get_dummies(df_2[['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'PhoneService',
            'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup',
-           'TechSupport',
+           'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies',
            'Contract', 'PaperlessBilling', 'PaymentMethod','tenure_group']])
     
     
@@ -93,10 +105,10 @@ def predict():
     
     if single==1:
         o1 = "This customer is likely to be churned!!"
-        o2 = "Confidence: {}".format(probablity*100)
+        o2 = "{}".format(probablity*100)
     else:
         o1 = "This customer is likely to continue!!"
-        o2 = "Confidence: {}".format(probablity*100)
+        o2 = "{}".format(probablity*100)
         
     return render_template('home.html', output1=o1, output2=o2, 
                            query1 = request.form['query1'], 
@@ -110,10 +122,14 @@ def predict():
                            query9 = request.form['query9'], 
                            query10 = request.form['query10'], 
                            query11 = request.form['query11'], 
+                           query12 = request.form['query12'], 
                            query13 = request.form['query13'], 
+                           query14 = request.form['query14'], 
+                           query15 = request.form['query15'], 
                            query16 = request.form['query16'], 
                            query17 = request.form['query17'],
                            query18 = request.form['query18'], 
                            query19 = request.form['query19'])
     
 app.run()
+
--- myenv/bin/activate.csh
+++ myenv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/yatish/Desktop/End-End-Telecommunication-Churn/myenv"
+setenv VIRTUAL_ENV "/Users/yatish/Desktop/MLProject-ChurnPrediction/myenv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
