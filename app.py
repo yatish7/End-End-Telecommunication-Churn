29d570ad64c7554db3bdea838e91e84b2107b50e
yatish7 <yatish.manne09@gmail.com>
2023-10-05 22:03:57 +0530

    Completed the project

--- app.py
+++ app.py
@@ -1,135 +1,37 @@
-import sys
-sys.path.append('/Users/yatish/miniconda3/envs/myenv/lib/python3.10/site-packages')
+from flask import Flask, render_template, request
+import numpy as np
+import joblib
 
+app = Flask(__name__)
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier 
-from sklearn import metrics
-from flask import Flask, request, render_template
-import pickle
+model = joblib.load('model.pkl')
 
-
-app = Flask("__name__")
-
-df_1=pd.read_csv("first_telc.csv")
-
-q = ""
-
-@app.route("/")
-def loadPage():
-	return render_template('home.html', query="")
-
-
-@app.route("/", methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def predict():
-    
-    '''
-    SeniorCitizen
-    MonthlyCharges
-    TotalCharges
-    gender
-    Partner
-    Dependents
-    PhoneService
-    MultipleLines
-    InternetService
-    OnlineSecurity
-    OnlineBackup
-    DeviceProtection
-    TechSupport
-    StreamingTV
-    StreamingMovies
-    Contract
-    PaperlessBilling
-    PaymentMethod
-    tenure
-    '''
-    
+    if request.method == 'POST':
+        Gender = int(request.form['Gender'])
+        SeniorCitizen = int(request.form['SeniorCitizen'])
+        Tenure = int(request.form['Tenure'])
+        InternetService = int(request.form['InternetService'])
+        Contract = int(request.form['Contract'])
+        PaperlessBilling = int(request.form['PaperlessBilling'])
+        MonthlyCharges = float(request.form['MonthlyCharges'])
+        TotalCharges = int(request.form['TotalCharges'])
 
-    
-    inputQuery1 = request.form['query1']
-    inputQuery2 = request.form['query2']
-    inputQuery3 = request.form['query3']
-    inputQuery4 = request.form['query4']
-    inputQuery5 = request.form['query5']
-    inputQuery6 = request.form['query6']
-    inputQuery7 = request.form['query7']
-    inputQuery8 = request.form['query8']
-    inputQuery9 = request.form['query9']
-    inputQuery10 = request.form['query10']
-    inputQuery11 = request.form['query11']
-    inputQuery12 = request.form['query12']
-    inputQuery13 = request.form['query13']
-    inputQuery14 = request.form['query14']
-    inputQuery15 = request.form['query15']
-    inputQuery16 = request.form['query16']
-    inputQuery17 = request.form['query17']
-    inputQuery18 = request.form['query18']
-    inputQuery19 = request.form['query19']
+        new_arr = np.array([[Gender, SeniorCitizen, Tenure, InternetService, Contract, PaperlessBilling, MonthlyCharges, TotalCharges]])
 
-    model = pickle.load(open("model.sav", "rb"))
-    
-    data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5, inputQuery6, inputQuery7, 
-             inputQuery8, inputQuery9, inputQuery10, inputQuery11, inputQuery12, inputQuery13, inputQuery14,
-             inputQuery15, inputQuery16, inputQuery17, inputQuery18, inputQuery19]]
-    
-    new_df = pd.DataFrame(data, columns = ['SeniorCitizen', 'MonthlyCharges', 'TotalCharges', 'gender', 
-                                           'Partner', 'Dependents', 'PhoneService', 'MultipleLines', 'InternetService',
-                                           'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport',
-                                           'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling',
-                                           'PaymentMethod', 'tenure'])
-    
-    df_2 = pd.concat([df_1, new_df], ignore_index = True) 
-    # Group the tenure in bins of 12 months
-    labels = ["{0} - {1}".format(i, i + 11) for i in range(1, 72, 12)]
-    
-    df_2['tenure_group'] = pd.cut(df_2.tenure.astype(int), range(1, 80, 12), right=False, labels=labels)
-    #drop column customerID and tenure
-    df_2.drop(columns= ['tenure'], axis=1, inplace=True)   
-    
-    
-    
-    
-    new_df__dummies = pd.get_dummies(df_2[['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'PhoneService',
-           'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup',
-           'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies',
-           'Contract', 'PaperlessBilling', 'PaymentMethod','tenure_group']])
-    
-    
-    #final_df=pd.concat([new_df__dummies, new_dummy], axis=1)
-        
-    
-    single = model.predict(new_df__dummies.tail(1))
-    probablity = model.predict_proba(new_df__dummies.tail(1))[:,1]
-    
-    if single==1:
-        o1 = "This customer is likely to be churned!!"
-        o2 = "{}".format(probablity*100)
-    else:
-        o1 = "This customer is likely to continue!!"
-        o2 = "{}".format(probablity*100)
-        
-    return render_template('home.html', output1=o1, output2=o2, 
-                           query1 = request.form['query1'], 
-                           query2 = request.form['query2'],
-                           query3 = request.form['query3'],
-                           query4 = request.form['query4'],
-                           query5 = request.form['query5'], 
-                           query6 = request.form['query6'], 
-                           query7 = request.form['query7'], 
-                           query8 = request.form['query8'], 
-                           query9 = request.form['query9'], 
-                           query10 = request.form['query10'], 
-                           query11 = request.form['query11'], 
-                           query12 = request.form['query12'], 
-                           query13 = request.form['query13'], 
-                           query14 = request.form['query14'], 
-                           query15 = request.form['query15'], 
-                           query16 = request.form['query16'], 
-                           query17 = request.form['query17'],
-                           query18 = request.form['query18'], 
-                           query19 = request.form['query19'])
-if __name__=="__main__":    
-    app.run(host="0.0.0.0",port=8080)
+        new_output = model.predict(new_arr)
+        probabilities = model.predict_proba(new_arr)
 
+        if new_output[0] == 1:
+            predicted_result = "Customer is likely to subscribe!"
+        else:
+            predicted_result = "Customer is unlikely to subscribe"
+
+        confidence = 100*probabilities[0][1]
+
+        return render_template('home.html', predicted_result=predicted_result, confidence=confidence)
+    return render_template('home.html', output1="", output2="", query1="", query2="", query3="", query4="", query5="", query6="", query7="", query8="")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
